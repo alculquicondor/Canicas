f32112f385965415bf560bd8f716524dc89f38a2
Aldo Culquicondor <alculquicondor@gmail.com>
2015-09-03 11:43:22 -0500

    First commit:
    - Scene
    - Materials
    - Physics Materials
    - Camera rotation and following
    - Terrain

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	public float radioOffset, heightOffset, angle;
+
+	void Start () {
+		Vector3 offset = transform.position - player.transform.position;
+		radioOffset = Mathf.Sqrt(offset.x * offset.x + offset.z * offset.z);
+		heightOffset = offset.y;
+		angle = Mathf.PI;
+	}
+
+	void FixedUpdate() {
+		angle += Input.GetAxis ("Horizontal") * 7e-3f;
+	}
+	
+	void LateUpdate () {
+		transform.position = player.transform.position +
+			new Vector3(Mathf.Sin (angle) * radioOffset,
+			            heightOffset,
+			            Mathf.Cos (angle) * radioOffset);
+		transform.LookAt(player.transform.position);
+	}
+}
