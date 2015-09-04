9b0b00455966fea46c3cf9f232ad30d897fc8a7d
Adolfo Tamayo <adolfo.tamayo@ucsp.edu.pe>
2015-09-03 23:17:43 -0500

    Added movement to the player object, tinkered with speed and physical values for better animation

---
+++ Assets/Scripts/DragAndMove.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragAndMove : MonoBehaviour {
+
+	private RaycastHit rayHitStart;
+	private RaycastHit rayHitEnd;
+	private Rigidbody rb;
+
+	public float speed;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void OnMouseDown(){
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		bool hit = Physics.Raycast (ray.origin, ray.direction, out rayHitStart);
+		if (hit) {
+			Debug.Log("START:");
+			Debug.Log(rayHitStart.point);
+		}
+	}
+
+	void OnMouseUp(){
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		bool hit = Physics.Raycast (ray.origin, ray.direction, out rayHitEnd);
+		if (hit) {
+			Vector3 movement = new Vector3(
+				rayHitStart.point.x - rayHitEnd.point.x,
+				0,
+				rayHitStart.point.z - rayHitEnd.point.z);
+
+			Debug.Log("End:");
+			Debug.Log(rayHitEnd.point);
+			Debug.Log("Movement:");
+			Debug.Log(movement);
+
+			
+			rb.AddForce(movement * speed);
+
+		}
+	}
+	void Update () {
+
+	}
+}
