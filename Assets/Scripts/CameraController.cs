95caa04e4d0cceadd55bc2a2b0c858dbec801fa6
Aldo Culquicondor <alculquicondor@gmail.com>
2015-09-10 10:26:18 -0500

    adding vertical control of Camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,15 +7,17 @@ public class CameraController : MonoBehaviour {
 	public static int gameScore;
 	public float speed;
 
-	private float radioOffset, heightOffset, angle,
-		          startingAngle, startingMouseX;
+	private float radioOffset, fullOffset, sideAngle, verticalAngle,
+		          startingSideAngle, startingMouseX,
+	              startingVerticalAngle, startingMouseY;
 	private bool movingCamera;
 
 	void Start () {
 		Vector3 offset = transform.position - player.transform.position;
 		radioOffset = Mathf.Sqrt(offset.x * offset.x + offset.z * offset.z);
-		heightOffset = offset.y;
-		angle = Mathf.PI;
+		fullOffset = Mathf.Sqrt (radioOffset * radioOffset + offset.y * offset.y);
+		sideAngle = Mathf.PI;
+		verticalAngle = Mathf.Asin(offset.y / fullOffset);
 		gameScore = 0;
 		movingCamera = false;
 	}
@@ -23,11 +25,14 @@ public class CameraController : MonoBehaviour {
 	void Update () {
 		if (Input.GetMouseButtonDown(1)) {
 			movingCamera = true;
-			startingAngle = angle;
+			startingSideAngle = sideAngle;
+			startingVerticalAngle = verticalAngle;
 			startingMouseX = Input.mousePosition.x;
+			startingMouseY = Input.mousePosition.y;
 		}
 		if (movingCamera) {
-			angle = startingAngle + (Input.mousePosition.x - startingMouseX) * speed;
+			sideAngle = startingSideAngle + (Input.mousePosition.x - startingMouseX) * speed;
+			verticalAngle = startingVerticalAngle - (Input.mousePosition.y - startingMouseY) * speed / 2;
 		}
 		if (movingCamera && Input.GetMouseButtonUp(1)) {
 			movingCamera = false;
@@ -36,9 +41,9 @@ public class CameraController : MonoBehaviour {
 
 	void LateUpdate () {
 		transform.position = player.transform.position +
-			new Vector3(Mathf.Sin (angle) * radioOffset,
-			            heightOffset,
-			            Mathf.Cos (angle) * radioOffset);
+			new Vector3(Mathf.Sin (sideAngle) * radioOffset,
+			            Mathf.Sin (verticalAngle) * fullOffset,
+			            Mathf.Cos (sideAngle) * radioOffset);
 		transform.LookAt(player.transform.position);
 	}
 }
